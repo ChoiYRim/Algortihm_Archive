1c42d78207cac76c99c1542c861bb972955f4827
ChoiYRim <zerorimchoi@gmail.com>
2021-11-22 15:09:46 +0900

    Back Tracking

---
+++ Back Tracking/[BOJ] 15666_BackTracking.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int N,M;
+vector<int> num;
+map<vector<int>,bool> m;
+
+void DFS(int idx,int depth,vector<int> v)
+{
+    if(depth >= M)
+    {
+        if(m.find(v) != m.end())
+            return;
+        
+        m.insert({v,true});
+        for(int i = 0; i < M; i++)
+            cout << v[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    
+    for(int i = idx; i < (int)num.size(); i++)
+    {
+        v.push_back(num[i]);
+        DFS(i,depth+1,v);
+        v.pop_back();
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    for(int i = 0; i < N; i++)
+    {
+        int tmp; cin >> tmp;
+        num.push_back(tmp);
+    }
+    
+    sort(num.begin(),num.end());
+    
+    DFS(0,0,{});
+    return 0;
+}
