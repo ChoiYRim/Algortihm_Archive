985398d6fe9ccd314676679fe6fc375bbf4522fc
ChoiYRim <zerorimchoi@gmail.com>
2021-11-24 15:55:22 +0900

    Math : 1242번 답지 참조

--- Math/[BOJ] 1242_Math.cpp
+++ Math/[BOJ] 1242_Math.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 int N,M,K;
 
+int game(int total,int target)
+{
+    int out_turn = (K % total != 0 ? K % total : total);
+    
+    if(out_turn == target || total == 1) // 재귀 탈출문
+        return 1;
+    
+    if(target >= out_turn)
+        target = target - out_turn;
+    else
+        target = (total - out_turn) + target;
+    
+    return game(total-1,target)+1;
+}
+
 int main()
 {
     cin >> N >> K >> M;
+    cout << game(N,M) << '\n';
     return 0;
 }
