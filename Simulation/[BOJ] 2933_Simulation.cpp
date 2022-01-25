81d6c88eb12ff7bbeb6352847660a5d8fe750e5c
ChoiYRim <zerorimchoi@gmail.com>
2022-01-25 17:08:39 +0900

    2933번 그냥 다시 풀어봄

--- Simulation/[BOJ] 2933_Simulation.cpp
+++ Simulation/[BOJ] 2933_Simulation.cpp
@@ -1,144 +1,204 @@
-// reference : https://rile1036.tistory.com/133
-
-#include<iostream>
-#include<vector>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <set>
 
 using namespace std;
 
-#define MAX 101
+int R,C,N;
+vector<int> stick;
+vector<string> map;
+int dy[4] = {-1,0,1,0};
+int dx[4] = {0,1,0,-1};
 
-int R, C, N;
-vector <int> stick;
-vector <pair <int, int>> cluster;
-char arr[MAX][MAX];
-bool visit[MAX][MAX];
+inline bool range(int y,int x) { return ((y >= 0 && y < R) && (x >= 0 && x < C)); }
 
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-
-void clust_Down() {
-	bool cluster_visit[MAX][MAX];
-	memset(cluster_visit, false, sizeof(cluster_visit));
-
-	for (int i = 0; i < cluster.size(); i++) {
-		int x = cluster[i].first;
-		int y = cluster[i].second;
-		if (x == R - 1) 
-			return;
-		else {
-			arr[x][y] = '.';
-			visit[x][y] = false;
-		}
-	}
-
-	for (int i = 0; i < cluster.size(); i++) {
-		int x = cluster[i].first;
-		int y = cluster[i].second;
-
-		arr[x + 1][y] = 'x';
-		visit[x + 1][y] = true;
-		cluster_visit[x + 1][y] = true;
-		cluster[i].first += 1;
-	}
-
-	for (int i = 0; i < cluster.size(); i++) {
-		int x = cluster[i].first;
-		int y = cluster[i].second;
-
-		if (x == R - 1)
-			return;
-		if (arr[x + 1][y] == 'x' && !cluster_visit[x + 1][y])
-			return;
-	}
-	clust_Down();
-	return;
+bool comp(const pair<int,int>& p1,const pair<int,int>& p2)
+{
+    if(p1.first == p2.first)
+        return p1.second < p2.second;
+    return p1.first < p2.first;
 }
 
-void DFS(int x, int y) {
-	cluster.push_back(make_pair(x, y));
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-
-		if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
-		if (arr[nx][ny] == 'x' && !visit[nx][ny]) {
-			visit[nx][ny] = true;
-			DFS(nx, ny);
-		}
-	}
+void input()
+{
+    cin >> R >> C;
+    for(int y = 0; y < R; y++)
+    {
+        string tmp; cin >> tmp;
+        map.push_back(tmp);
+    }
+    cin >> N;
+    for(int i = 0; i < N; i++)
+    {
+        int tmp; cin >> tmp;
+        stick.push_back(tmp);
+    }
 }
 
-void clust() {
-	for (int i = R - 1; i >= 0; i--) {
-		for (int j = 0; j < C; j++) {
-			if (arr[i][j] == 'x' && !visit[i][j]) {
-				cluster.clear();
-				visit[i][j] = true;
-				DFS(i, j);
-				clust_Down();
-			}
-		}
-	}
+auto Throw(bool turn,int height)
+{
+    if(!turn)
+    {
+        for(int col = 0; col < C; col++)
+        {
+            if(map[height][col] == 'x')
+            {
+                map[height][col] = '.';
+                break;
+            }
+        }
+    }
+    else
+    {
+        for(int col = C-1; col >= 0; col--)
+        {
+            if(map[height][col] == 'x')
+            {
+                map[height][col] = '.';
+                break;
+            }
+        }
+    }
 }
 
-void Throw(int height, bool flag) {
-	height = R - height;
-
-	if (flag) { // 왼
-		for (int i = 0; i < C; i++) {
-			if (arr[height][i] == 'x') {
-				arr[height][i] = '.';
-				break;
-			}
-		}
-	}
-	if (!flag) { // 오
-		for (int i = C - 1; i >= 0; i--) {
-			if (arr[height][i] == 'x') {
-				arr[height][i] = '.';
-				break;
-			}
-		}
-	}
-	return;
+void dfs(int y,int x,vector<vector<bool>>& visit)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        int ny = y+dy[i];
+        int nx = x+dx[i];
+        
+        if(!range(ny,nx))
+            continue;
+        
+        if(map[ny][nx] == 'x' && !visit[ny][nx])
+        {
+            visit[ny][nx] = 1;
+            dfs(ny,nx,visit);
+        }
+    }
 }
 
-void init() {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
+void down_spaces(vector<set<pair<int,int>>>& spaces)
+{
+    for(auto i = 0; i < spaces.size(); i++)
+    {
+        int diff = 0x7fffffff;
+        set<pair<int,int>> space = spaces[i];
+        
+        for(auto& s : space)
+        {
+            for(int j = 1; ; j++)
+            {
+                int y = s.first+j;
+                int x = s.second;
+                
+                if(space.find({y,x}) != space.end())
+                    break;
+                if(!range(y,x))
+                {
+                    diff = min(diff,j-1);
+                    break;
+                }
+                if(map[y][x] == 'x')
+                {
+                    diff = min(diff,j-1);
+                    break;
+                }
+            }
+        }
+        
+        for(auto& s : space)
+            map[s.first][s.second] = '.';
+        for(auto& s : space)
+            map[s.first+diff][s.second] = 'x';
+    }
 }
 
-int main() {
-	init();
-	cin >> R >> C;
-	
-	for (int i = 0; i < R; i++)
-		for (int j = 0; j < C; j++)
-			cin >> arr[i][j];
+void down()
+{
+    vector<vector<bool>> visit(R,vector<bool>(C,0));
+    vector<set<pair<int,int>>> spaces;
+    
+    // 땅에 붙어있는 클러스터들 찾기
+    for(int x = 0; x < C; x++)
+    {
+        if(map[R-1][x] == 'x' && !visit[R-1][x])
+        {
+            visit[R-1][x] = 1;
+            dfs(R-1,x,visit);
+        }
+    }
+    
+    // 공중에 떠 있는 클러스터들 찾기
+    for(int y = R-1; y >= 0; y--)
+    {
+        for(int x = 0; x < C; x++)
+        {
+            if(!visit[y][x] && map[y][x] == 'x')
+            {
+                set<pair<int,int>> space;
+                queue<pair<int,int>> q;
+                
+                q.push({y,x});
+                visit[y][x] = 1;
+                space.insert({y,x});
+                while(!q.empty())
+                {
+                    pair<int,int> cur = q.front();
+                    q.pop();
+                    
+                    for(int i = 0; i < 4; i++)
+                    {
+                        int ny = cur.first+dy[i];
+                        int nx = cur.second+dx[i];
+                        
+                        if(!range(ny,nx))
+                            continue;
+                        if(!visit[ny][nx] && map[ny][nx] == 'x')
+                        {
+                            space.insert({ny,nx});
+                            q.push({ny,nx});
+                            visit[ny][nx] = 1;
+                        }
+                    }
+                }
+                
+                spaces.push_back(space);
+            }
+        }
+    }
+    
+    // 클러스터 내리기
+    down_spaces(spaces);
+}
 
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		int stick_num; cin >> stick_num;
-		stick.push_back(stick_num);
-	}
-	for (int i = 0; i < stick.size(); i++) {
-		memset(visit, false, sizeof(visit));
-		if (i % 2 == 0)
-			Throw(stick[i], true);
-		else
-			Throw(stick[i], false);
-		clust();
-	}
+void solve()
+{
+    for(int turn = 0; turn < N; turn++)
+    {
+        int height = R-stick[turn];
+        
+        Throw(turn%2,height); // throw a stick
+        down();
+    }
+}
 
-	for (int i = 0; i < R; i++) {
-		for (int j = 0; j < C; j++) {
-			cout << arr[i][j];
-		}
-		cout << endl;
-	}
-	return 0;
+void output()
+{
+    for(int y = 0; y < R; y++)
+        cout << map[y] << endl;
+}
+
+int main(int argc,char* argv[])
+{
+    input();
+    
+    solve();
+    
+    output();
+    return 0;
 }
